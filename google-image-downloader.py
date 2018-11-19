1a37fc7629148b0957c28a8839e937e861f59ac9
GiuseppeBaldini <40420789+GiuseppeBaldini@users.noreply.github.com>
2018-11-19 18:11:31 +0100

    Initial commit

---
+++ google-image-downloader.py
@@ -0,0 +1,13 @@
+#! python3
+
+# Google Images Downloader using Selenium
+
+# TODO: Specify path / url / extensions
+
+# TODO: Input arguments
+
+# TODO: Use Selenium to access Google Images
+
+# TODO: Download images in specified folder
+
+# TODO: [OPTIONAL] Summary (number of images / path / size)

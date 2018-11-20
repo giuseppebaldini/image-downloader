fc801ae62d66bbce3ef8cdd9d5bd3f1f07ca1182
GiuseppeBaldini <40420789+GiuseppeBaldini@users.noreply.github.com>
2018-11-20 17:31:06 +0100

    Initial commit

--- google-image-downloader.py
+++ google-image-downloader.py
@@ -2,7 +2,24 @@
 
 # Google Images Downloader using Selenium
 
-# TODO: Specify path / url / extensions
+import os
+import sys
+import json
+import urllib
+import selenium
+
+search_term = sys.argv[1]
+
+try:
+    num_images = sys.argv[2]
+except IndexError:
+    num_images = 10             # Default
+
+# TODO: Specify download path / url / extensions
+
+download_dir = '...'
+url = 'https://www.images.google.com/search?tbm=isch&q=' + search_term
+extensions = ["jpg", "jpeg", "png", "gif"]
 
 # TODO: Input arguments
 

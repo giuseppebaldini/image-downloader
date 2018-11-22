2976be5103e795e847f3112e993030db1e559060
GiuseppeBaldini <40420789+GiuseppeBaldini@users.noreply.github.com>
2018-11-22 13:17:34 +0100

    Initial commit

--- google-image-downloader.py
+++ google-image-downloader.py
@@ -2,6 +2,8 @@
 
 # Google Images Downloader using Selenium
 
+# Usual: Check - Comments - Readme
+
 import os
 import sys
 import json
@@ -9,19 +11,20 @@ import urllib3
 from selenium import webdriver
 
 # Initial arguments
-search_term = sys.argv[1]
+search_term = ' '.join(sys.argv[1:-1]).title()
+search_line = search_term.replace(" ","_")
 
 try:
-    num_images = int(sys.argv[2])
-except IndexError:
-    num_images = 10             # Default
+    num_images = int(sys.argv[-1])
+except (IndexError, ValueError):
+    num_images = 10                     # Default
 
 http = urllib3.PoolManager()
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # Specify download path / url / extensions
 main_dir = r'C:\Users\Giuseppe\Documents\Images'
-download_dir = main_dir + '\\' + search_term.replace(" ","_")
+download_dir = main_dir + '\\' + search_line
 
 if not os.path.exists(download_dir):
     os.makedirs(download_dir)
@@ -30,15 +33,15 @@ url = 'https://www.google.com/search?tbm=isch&q=' + search_term
 extensions = [".png", ".jpg", ".jpeg", ".gif", ".bmp"]
 
 # Use Selenium to access Google Images
-driver = webdriver.Firefox()
-driver.get(url)
-
-header = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)' }
+os.environ['MOZ_HEADLESS'] = '1'
+browser = webdriver.Firefox()
+browser.get(url)
 
 counter = 0
+size = 0
 
 # Find images using XPath
-images = driver.find_elements_by_xpath('//div[contains(@class,"rg_meta")]')
+images = browser.find_elements_by_xpath('//div[contains(@class,"rg_meta")]')
 
 for img in images:
 
@@ -52,13 +55,21 @@ for img in images:
 
     img_req = http.request('GET', img_url, preload_content = False)
 
-    with open(download_dir + ' ' + str(counter) + img_ext, 'wb') as f:
+    file_name = os.path.join(download_dir, search_line + str(counter) + img_ext)
+
+    with open(file_name, 'wb') as f:
         img_file = img_req.read()
         f.write(img_file)
+
+    size += os.path.getsize(file_name)
 
     if counter >= num_images:
         break
 
     img_req.release_conn()
 
-# TODO: [OPTIONAL] Summary (number of images / path / size)
+print('''Download completed. %d images downloaded in %s.
+Total size: %d MB.
+''' % (counter, download_dir, (size / (1024 * 1024))))
+
+browser.quit()
